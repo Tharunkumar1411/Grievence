7388000484a68c8e6a91ba06f7fa28c2f46c86ce
Tharunkumar001 <tharung1411@gmail.com>
2021-10-16 18:45:14 +0530

    resolve jwt issue

--- grievence/controller/AuthFunctions.js
+++ grievence/controller/AuthFunctions.js
@@ -10,6 +10,7 @@ dotenv.config();
 exports.jwt = (async(req, res) => {
 
     var token = req.body.jwt;
+    console.log(token);
 
     if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
 
@@ -17,7 +18,8 @@ exports.jwt = (async(req, res) => {
         if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
 
 
-        const foundUser = User.findOne({ "_id": decoded.id }).then((result) => {
+        const foundUser = User.findOne({ "email": decoded.email }).then((result) => {
+            // console.log(result);
             res.send(result.email)
         })
 
@@ -43,10 +45,8 @@ exports.signIn = (async(req, res) => {
                         res.status(404).send({auth: false});
                     }
                 })
-            
         } else {
             User.create({
-
                     name: req.body.name,
                     email: req.body.email,
                     password: req.body.password,
